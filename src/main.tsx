0a0be3aa67d5a1904ab0657e8e28b25d72123467
oneOfThePeakyBlinders <magomedkuriev2001@gmail.com>
2024-03-18 03:45:21 +0300

    did some updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import {AdaptivityProvider, ConfigProvider} from "@vkontakte/vkui";
-import bridge from '@vkontakte/vk-bridge';
 import React from "react";
 
-bridge.send("VKWebAppInit");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <ConfigProvider>
